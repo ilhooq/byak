cd4a1379aad7d3dff02f9cdf0ceb258322e5e79c
ilhooq <contact@sphilip.com>
2013-05-01 13:50:43 +0200

    Add license informations

--- src/prng.c
+++ src/prng.c
@@ -20,6 +20,23 @@ Following the feature list, as tested by Heinz.
 
 #include "prng.h"
 #define rot(x,k) (((x)<<(k))|((x)>>(64-(k))))
+/**
+* Byak, a UCI chess engine.
+* Copyright (C) 2013  Sylvain Philip
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 static U64 a;
 static U64 b;
