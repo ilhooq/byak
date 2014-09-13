56e3029f76d12c9f3716a1428cf20e81d7c044ee
ilhooq <contact@sphilip.com>
2014-09-13 15:39:23 +0200

    Revert changes in eval because score wasn't well calculated

--- src/eval.c
+++ src/eval.c
@@ -42,20 +42,13 @@ int eval_position()
 	* //where who2move = 1 for white, and who2move = -1 for black).
 	*/
 	int who2move = (pos.side == WHITE) ? 1 : -1;
-	int material[10];
-	Piece i;
-	for (i=0; i < 10; i++) {
-		material[i] = COUNT_MATERIAL(i);
-	}
-
-	// Material eval in centipawn;
 	int score = 0;
 
-	score += 100 * (material[P] - material[p]);
-	score += 900 * (material[Q] - material[q]);
-	score += 300 * (material[N] - material[n]);
-	score += 300 * (material[B] - material[b]);
-	score += 600 * (material[R] - material[r]);
+	score += 10 * (COUNT_MATERIAL(P) - COUNT_MATERIAL(p));
+	score += 90 * (COUNT_MATERIAL(Q) - COUNT_MATERIAL(q));
+	score += 30 * (COUNT_MATERIAL(N) - COUNT_MATERIAL(n));
+	score += 30 * (COUNT_MATERIAL(B) - COUNT_MATERIAL(b));
+	score += 60 * (COUNT_MATERIAL(R) - COUNT_MATERIAL(r));
 
 	return score * who2move;
 }
